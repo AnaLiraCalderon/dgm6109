4a24be730c82de5467d570904972299e485cae55
AnaLiraCalderon <liracalderon.a@northeastern.edu>
2026-01-12 19:43:38 -0800

    Uploaded index file
    temperature homework

---
+++ lab1/temp-conversion.js
@@ -0,0 +1,7 @@
+let fahrenheit = 57.2 // 0c - 273.15k // Temperature represents the average daytime temperature in Huaraz, Peru, during April 2025
+let celsius = (fahrenheit - 32) * 5 / 9
+let kelvin = (fahrenheit + 459.67) * 5 / 9
+
+console.log("Temperature (celsius):" + celsius);
+console.log("Temperature (kelvin):" + kelvin);
+console.log("Temperature (fahrenheit):" + fahrenheit);
